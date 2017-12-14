0be2a0747cf6ecdc89058f91b011317304ec1dae
Amy Carney <amy.carney@alaska.gov>
2017-12-14 01:26:39 +0000

    automatically backed up by learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 def fizzbuzz(int)
+  binding.pry
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif init % 5 == 0
+  if init % 5 == 0
     "Buzz"
-  elsif init % 15 == 0
+  if init % 15 == 0
     "FizzBuzz"
   else
     nil
